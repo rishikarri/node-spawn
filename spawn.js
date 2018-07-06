5cdb057a8210de5b61e7e34d3ce3ede2228e13fb
Rishi Karri <rishi.karri@turner.com>
2018-07-05 21:04:17 -0400

    pipe

--- spawn.js
+++ spawn.js
@@ -1,8 +1,16 @@
 const { spawn } = require('child_process');
 
-const child = spawn('pwd');
+// const child = spawn('pwd');
 
-child.on('exit', function(code, signal) {
-    console.log(code, 'code');
-    console.log(signal, 'signlal');
-})
+// child.on('exit', function(code, signal) {
+//     console.log(code, 'code');
+//     console.log(signal, 'signlal');
+// })
+
+const child = spawn('wc');
+
+process.stdin.pipe(child.stdin)
+
+child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+});
